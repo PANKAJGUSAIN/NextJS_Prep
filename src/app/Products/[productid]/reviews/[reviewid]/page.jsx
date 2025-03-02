c4d2978de752436a488fec86e43e8c627fe600e9
Pankaj Gusain <gusainpankaj1999@gmail.com>
2025-03-02 20:28:59 +0530

    create not-found.jsx and used notfound() to call specific not-found.jsx component for that route

--- src/app/Products/[productid]/reviews/[reviewid]/page.jsx
+++ src/app/Products/[productid]/reviews/[reviewid]/page.jsx
@@ -1,6 +1,11 @@
+import { notFound } from "next/navigation";
+
 export default async function (props){
     const result = await props.params;
     console.log(result)
+    if( parseInt(result.reviewid) > 1000){
+        notFound();
+    }
     return(
         <>
             review for Product{result.productid} and review {result.reviewid}
