0fe022e9df2d1b9d306cd895ca32aaa6bd39ea9a
Pankaj Gusain <gusainpankaj1999@gmail.com>
2025-03-03 11:20:55 +0530

    create multiple rootLayout and unquie to routes using RouteGroups

---
+++ src/app/(marketing)/layout.jsx
@@ -0,0 +1,13 @@
+
+
+export default function Layout({ children }) {
+  return (
+        <>
+        <div className="font-black text-2xl bg-blue-500 w-full h-[200px] mb-2 flex items-center justify-center">Header - for Marketing layout</div>
+        <div>
+          {children}
+        </div>
+        <div className="font-black text-2xl  bg-blue-900 w-full h-[200px] mt-2 flex items-center justify-center" >Footer - for Marketing layout</div>
+        </>
+  )
+}
--- src/app/layout.js
+++ src/app/layout.js
@@ -10,11 +10,8 @@ export default function Layout({ children }) {
     <html lang="en">
       <body className='h-screen w-screen p-0 m-0'>
         <>
-        <div className="font-black text-2xl bg-amber-100 w-full h-[200px] mb-2 flex items-center justify-center">Header - from rootlayout</div>
-        <div>
+      
           {children}
-        </div>
-        <div className="font-black text-2xl bg-amber-400 w-full h-[200px] mt-2 flex items-center justify-center" >Footer - from rootlayout</div>
         </>
         </body>
     </html>
