891c0da751ca0394c1d572d0f867ce6f15b211f4
Pankaj Gusain <gusainpankaj1999@gmail.com>
2025-03-03 12:26:50 +0530

    create dynamic  metadata using generateMetadata function

--- src/app/Products/[productid]/page.jsx
+++ src/app/Products/[productid]/page.jsx
@@ -1,3 +1,15 @@
+export const generateMetadata = async(props) => {
+    const result = await props.params
+    const title = await new Promise((resolve, reject) => {
+          setTimeout(() => {
+            resolve(`iphone ${result.productid}`)
+          }, 2000);     
+    })
+    return {
+        title : `Product ${title} - ${result.productid}`
+    }
+}
+
 
 export default async function productid(props){
     const fetchedparams = await props.params ;
