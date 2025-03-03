63c2519eb0793ad63fa4fa190fa61a3dd43ddaf8
Pankaj Gusain <gusainpankaj1999@gmail.com>
2025-03-03 12:46:03 +0530

    create dynamic route uning <Link>  & replace prop use

--- src/app/Products/page.jsx
+++ src/app/Products/page.jsx
@@ -1,14 +1,16 @@
 import Link from "next/link";
 
 export default function productComponent() {
+    const productId = 101
     return (
         <>
             <h4 className="underline text-amber-900 text-2xl font-extrabold"><Link href="/">Home</Link></h4>
             <h1>
                 <ul>
-                    <li>Product 1</li>
-                    <li>Product 2</li>
-                    <li>Product 3</li>
+                    <li><Link href='/Products/1'>Product 1</Link></li>
+                    <li><Link href='/Products/2'>Product 2</Link></li>
+                    <li><Link href='/Products/3' replace>Product 3 with replace(overriding the history)</Link></li>
+                    <li><Link href={`/Products/${productId}`}>Dynamic Product link - {productId}</Link></li>
                 </ul>
             </h1>
         </>
