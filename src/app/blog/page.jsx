81e62ca1ce6e246698fa3049221f09dd5202c25e
Pankaj Gusain <gusainpankaj1999@gmail.com>
2025-03-04 22:52:19 +0530

    learned about loading.jsx file

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,7 +1,13 @@
-export default function blogRoot() {
-    return (
-        <>
-            Blogs Page
-        </>
-    )
+
+
+export default async function  blogRoot() {
+
+    // adding intentional delay to test loading.jsx is working or not 
+    await new Promise((resolve,reject) =>{
+        setTimeout(()=>{
+            resolve('intenional')
+        },5000)
+    } )
+
+    return <h1>Blog</h1>
 }
