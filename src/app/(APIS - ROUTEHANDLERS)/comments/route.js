959418f6a594b75d65f348c68b576241eb1e23d2
Pankaj Gusain <gusainpankaj1999@gmail.com>
2025-03-05 22:37:54 +0530

    RouteHANDLERS - post request and how to send response using Response object

--- src/app/(APIS - ROUTEHANDLERS)/comments/route.js
+++ src/app/(APIS - ROUTEHANDLERS)/comments/route.js
@@ -1,5 +1,22 @@
+import { headers } from "next/headers";
 import { comments } from "./data"
 
-export async function GET(){
+export async function GET() {
     return Response.json(comments)
+}
+
+export async function POST(request) {
+    const fetchcomment = await request.json();
+    const newComment = {
+        id: comments.length,
+        "text": fetchcomment.text
+    }
+    comments.push(newComment);
+    return new Response(
+        JSON.stringify(newComment),
+        {
+            headers: { "Content-Type": "application/json" },
+            status: 201,
+        }
+    )
 }
