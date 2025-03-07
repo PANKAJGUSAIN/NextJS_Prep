1ddbaf7afd202fb51156fa08c0821668f5c8fc85
Pankaj Gusain <gusainpankaj1999@gmail.com>
2025-03-07 12:21:09 +0530

    ROUTE HANDLER - PATCH AND DELETE REQUEST

--- src/app/(APIS - ROUTEHANDLERS)/comments/[id]/route.js
+++ src/app/(APIS - ROUTEHANDLERS)/comments/[id]/route.js
@@ -8,3 +8,29 @@ export async function GET(request , context) {
 
     return Response.json(comment)
 }
+
+export async function PATCH(request , context){
+    const { id } = await context.params 
+
+    const body = await request.json()
+
+    const { text } = body 
+
+    const index = comments.findIndex((comment)=>comment.id === parseInt(id))
+
+    comments[index].text = text 
+    
+    return Response.json(comments[index])
+
+}
+
+export async function DELETE(request , context){
+    
+    const { id } = await context.params 
+
+    const index = comments.findIndex((comment)=>comment.id === parseInt(id))
+    const deletedComment = comments[index]
+    comments.slice(index , 1)
+    return Response.json(deletedComment)
+
+}
