6ceba00112f37b1da73e1543a773c7f7f157362a
Pankaj Gusain <gusainpankaj1999@gmail.com>
2025-03-07 13:58:04 +0530

    ROUTE HANDLER - cookies in Route Handlers

--- src/app/(APIS - ROUTEHANDLERS)/profileApi/route.js
+++ src/app/(APIS - ROUTEHANDLERS)/profileApi/route.js
@@ -1,6 +1,9 @@
-import { headers } from "next/headers";
+import { cookies, headers } from "next/headers";
 
 export async function GET(request){
+
+
+    //headers
 
     //way1 
     console.log("Way1" , request.headers.get("authorization"))
@@ -14,7 +17,27 @@ export async function GET(request){
     console.log("way3" , headersList.get("authorization"))
 
 
+    //cookies
+
+    //way1
+    const theme = request.cookies.get("theme");
+
+    //way2
+    const cookieStore = await cookies();
+    cookieStore.set("resultsPerPage" , 20);
+    console.log("way1" , cookieStore)
+
+    console.log("cookie theme " , theme)
+
     
-    return new Response("hello")
+
+
+
+    return new Response("<h1>hello</h1>" , {
+        headers :{
+            "Content-Type":"text/html",
+            "Set-Cookie":"theme=dark"
+        }
+    })
     
 }
