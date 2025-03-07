7c24bf43a49396c0b9d6f5d1ce123c6eaf4f659f
Pankaj Gusain <gusainpankaj1999@gmail.com>
2025-03-07 14:17:44 +0530

    ROUTE HANDLER - redirect  in Route Handlers

--- src/app/(APIS - ROUTEHANDLERS)/comments/route.js
+++ src/app/(APIS - ROUTEHANDLERS)/comments/route.js
@@ -1,11 +1,27 @@
-import { comments} from "./data"
+import { redirect } from "next/dist/server/api-utils";
+import { comments } from "./data"
 
 export async function GET(request) {
-    console.log(request.nextUrl.searchParams)
-    const searchParams = request.nextUrl.searchParams
+    // Perform the redirection
+    // return new Response(null, {
+    //     status: 307,
+    //     headers: {
+    //         Location: "/api/v2/users",
+    //     },
+    // });
+;
+    // The following code will not be executed due to the redirection
+    console.log(request.nextUrl.searchParams);
+    const searchParams = request.nextUrl.searchParams;
     const query = searchParams.get("query");
-    const filtercomment = query ?  comments.filter(comment => comment.text.includes(query) ) : comments
-    return Response.json(filtercomment)
+    const filtercomment = query ? comments.filter(comment => comment.text.includes(query)) : comments;
+    return new Response(
+        JSON.stringify(filtercomment),
+        {
+            headers: { "Content-Type": "application/json" },
+            status: 200,
+        }
+    );
 }
 
 export async function POST(request) {
