9c9c302d203de5623e21191bba32e7930a6fffca
Pankaj Gusain <gusainpankaj1999@gmail.com>
2025-03-16 19:20:33 +0530

    Rendering - Server and client componsition pattern 1 - server-only

---
+++ Phase2-Rendering/src/app/client-route/page.js
@@ -0,0 +1,13 @@
+"use client";
+
+// import { serverSideFunction } from "@/utils/serverutils";
+
+export default function ClientRoute(){
+    // const result = serverSideFunction()
+    return (
+        <>
+            <h1>Client Route= {result}</h1>
+            
+        </>
+    )
+}
---
+++ Phase2-Rendering/src/app/server-route/page.js
@@ -0,0 +1,11 @@
+import { serverSideFunction } from "@/utils/serverutils";
+
+export default function ServerRoute(){
+    const result = serverSideFunction();
+    return (
+        <>
+            <h1>Server Route-{result}</h1>
+            
+        </>
+    )
+}
