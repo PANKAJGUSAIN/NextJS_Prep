6b12d2182f33148cd9913c90083e439cd96cfd5f
Pankaj Gusain <gusainpankaj1999@gmail.com>
2025-03-19 15:21:29 +0530

    Data-fetching - Data Mutation -Delete Product

--- phase3-data-fetching/src/app/products-db/page.js
+++ phase3-data-fetching/src/app/products-db/page.js
@@ -1,17 +1,23 @@
 import { getProducts } from "@/prisma-db";
 import Link from "next/link";
+import { removeProduct } from "./productsDelete";
 
 export default async function productsdbpage() {
     const product = await getProducts();
     return (
         <div className="p-4 max-w-md mx-auto bg-black rounded-xl shadow-md space-y-4">
             {product.map((item) => (
-                <div key={item.id}className="p-4 bg-white rounded-xl shadow-md space-y-4">
+                <div key={item.id} className="p-4 bg-white rounded-xl shadow-md space-y-4">
                     <div className="text-xl font-medium text-black">{item.id}</div>
                     <div className="text-lg font-semibold text-gray-900">
                         <Link href={`/products-db-update/${item.id}`}>{item.title}</Link></div>
                     <p className="text-gray-500">{item.description}</p>
                     <div className="text-lg font-bold text-gray-900">${item.price}</div>
+                    <form action={removeProduct.bind(null , item.id)}>
+                        <button className="bg-red-400 p-1 rounded-2xl">
+                            Delete
+                        </button>
+                    </form>
                 </div>
             ))}
         </div>
